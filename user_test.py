72308e315d1c98dabacf4212dd23bb351f72e5eb
PerisOduol618 <oduolpepe618@gmail.com>
2020-09-07 01:15:08 +0300

    Add test_save_user test case to test if the user object is saved into   the contact list

--- user_test.py
+++ user_test.py
@@ -1,14 +1,50 @@
-import unittest
-from user import User
+import unittest # Importing the unittest module
+from user import User # Importing the contact class
 
 class TestUser(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the user class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+
     def setUp(self):
-        self.user_credential = User('PerisOduol','peris254')
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user = User("PerisOduol","oduol254") # create contact object
+    
+    def tearDown(self):
+            '''
+            tearDown method that does clean up after each test case has run.
+            '''
+            User.user_list = []
 
     def test_init(self):
-        self.assertEqual(self.user_credential.user_name,'PerisOduol')
-        self.assertEqual(self.user_credential.password, 'peris254')
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+         
+        self.assertEqual(self.new_user.userName,"PerisOduol")
+        self.assertEqual(self.new_user.password,"oduol254")
 
-if __name__ == "__main__":
+    def test_save_user(self):
+        '''
+        test_save_user test case to test if the user object is saved into
+         the contact list
+        '''
+        self.new_user.save_user() # saving the new user
+
+    
+    def test_display_user(self):
+        '''
+        test case to test display user
+        '''
+        self.new_user.display_user()
+    
+if __name__ == '__main__':
     unittest.main()
+
     
