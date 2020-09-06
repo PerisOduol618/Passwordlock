d22496a914380acb1dca15d81ffcb882968aec0f
PerisOduol618 <oduolpepe618@gmail.com>
2020-09-06 12:13:32 +0300

    Add a method that saves the user
    test the method that saves the user

--- user.py
+++ user.py
@@ -1,5 +1,29 @@
+import pyperclip
+import random
+import string
+
 class User:
+    '''
+    Class User generate a instances of user cridentials.
+    '''
+
+    user_list = []
+
     def __init__(self, user_name, password):
+
+        '''
+        __init__ method that defines the properties for user objects 
+        '''
         self.user_name = user_name
         self.password = password
+
+    def save_user(self):
+        '''
+        user method that saves the objects in the user_list 
+        '''
+         
+        User.user_list.append(self)
+
+
+        
 
